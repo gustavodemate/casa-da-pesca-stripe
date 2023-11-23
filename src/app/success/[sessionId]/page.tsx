dc0f950c177e774bc69e01682bc9887258b4e678
gustavodemate <dema_dev@hotmail.com>
2023-11-23 12:09:29 -0300

    applying responsiveness to the purchase success page

--- src/app/success/[sessionId]/page.tsx
+++ src/app/success/[sessionId]/page.tsx
@@ -24,19 +24,22 @@ export default function Success({ params }: SuccessProps) {
   
   return (
     <div className="flex flex-col items-center justify-center mx-auto h-[656px]">
-      <div className="flex flex-row justify-center items-center">
-      <Check size={64} color="#00875f"/>
+      <div className="ml-12 flex items-center 2xl:m-0">
+        <Check size={64} color="#00875f"/>
         <h1 className="text-6xl text-green-700">Compra efetuada!</h1>
       </div>      
       <section className="text-2xl max-w-[560px] text-center mt-8 leading-4">
         <p>Obrigado pela confiança.</p>
         
+        
       </section>
 
-        <Link href="/" className="mt-4 text-xl text-green-600 no-underline font-medium hover:text-green-400 flex gap-3 items-center">
+      <Link href="/" className="mt-4 text-xl text-green-600 no-underline font-medium hover:text-green-400 flex gap-3 items-center">
            <MoveLeft /> 
            Voltar ao catálogo
-        </Link>      
+        </Link>   
+
+           
     </div>
   )
 }
